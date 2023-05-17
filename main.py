5d96fce8b5b7ed3cbd76403883c6665aa45b896f
Perminev <131896208+Perminev@users.noreply.github.com>
2023-05-17 19:23:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,10 @@
+from settings import settings
 import discord
-import random
-import time
-
-def flip_coin():
-    flip = random.randint(0, 2)
-    if flip == 0:
-        time.sleep(2)
-        return "ОРЕЛ"
-    else:
-        time.sleep(2)
-        return "РЕШКА"
-
-def gen_emodji():
-    emodji = ["\U0001f600", "\U0001f642", "\U0001F606", "\U0001F923"]
-    return random.choice(emodji)
-
-def gen_pass(pass_length):
-    elements = "+-/*!&$#?=@<>"
-    password = ""
-
-    for i in range(pass_length):
-        password += random.choice(elements)
-
-    return password
-
-#Основной код
+# import * - это тоже самое, что перечислить все файлы
+from bot_logic import gen_pass
+from bot_logic import gen_emodji
+from bot_logic import flip_coin
+from bot_logic import pi
 
 # Переменная intents - хранит привилегии бота
 intents = discord.Intents.default()
@@ -33,10 +13,14 @@ intents.message_content = True
 # Создаем бота в переменной client и передаем все привелегии
 client = discord.Client(command_prefix='$', intents=discord.Intents.default())
 
+
+# Когда бот будет готов, он напишет в консоли свое название!
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
 
+
+# Когда бот будет получать сообщение, он будет отправлять в этот же канал какие-то сообщения!
 @client.event
 async def on_message(message):
     if message.author == client.user:
@@ -50,8 +34,11 @@ async def on_message(message):
         await message.channel.send('> `$smile - генирирует случайное эмодзи`')
         await message.channel.send('> `$pass - генирирует случайный пароль`')
         await message.channel.send('> `$coin - подбрасывает монетку`')
+        await message.channel.send('> `$pi - число пи`')
     elif message.content.startswith('$smile'):
         await message.channel.send(gen_emodji())
+    elif message.content.startswith('$pi'):
+        await message.channel.send(pi())
     elif message.content.startswith('$coin'):
         await message.channel.send(flip_coin())
     elif message.content.startswith('$pass'):
@@ -59,4 +46,4 @@ async def on_message(message):
     else:
         await message.channel.send("Я не понимаю такую команду!")
 
-client.run("ENTER YOUR TOKEN HERE")
+client.run(settings["TOKEN"])
